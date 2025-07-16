dafe35049fc028cf7878879a8ae620287cff4755
akranish1 <akranish0@gmail.com>
2025-07-16 23:13:13 +0530

    React Route

--- Routing/app/src/App.js
+++ Routing/app/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link,Routes,Route,Navigate} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Link to='/'><h3>Home</h3></Link>
+    <Link to='/About'><h3>About</h3></Link>
+    <Link to='/Login'><h3>Login</h3></Link>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/About' element={<About/>}></Route>
+        <Route path='/Login' element={<Login/>}></Route>
+        {/* <Route path='/*' element={<h3>Error 404 Page not found</h3>}></Route> */}
+        <Route path='/*'element={<Navigate to='/'/>}></Route>
+      </Routes>
+    </>
   );
 }
 
