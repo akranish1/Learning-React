ce5c18114792fc52e5418fde1afea9a07c66d3ff
akranish1 <akranish0@gmail.com>
2025-07-19 00:54:14 +0530

    Learning reduxToolkit

--- Redux/redux/src/App.js
+++ Redux/redux/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment ,reset,incrementByAmount} from './feature/counter/counterSlice';
+import{useState} from 'react'
 
 function App() {
+  const [amount, setAmount]=useState(0)
+  //to take value out from store we use useSelector
+  const count=useSelector((state)=>state.counter.value) // value came from slice name:Counter
+  const dispatch=useDispatch();//kind of hook
+  function handleIncrementClick(){
+    dispatch(increment()) //came from reducer
+  }
+  function handleDecrementClick(){
+    dispatch(decrement())
+  }
+  function handleResetClick(){
+    dispatch(reset())
+  }
+  function handleIncClick(){
+    dispatch(incrementByAmount(amount))//payload
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+      <button onClick={handleIncrementClick}>+</button>
+      <p>Count: {count}</p>
+      <button onClick={handleDecrementClick}>-</button>
+      <button onClick={handleResetClick}>Reset</button>
+      <br/>
+      <br/>
+      <input type='Number' value={amount} placeholder="Enter amount"onChange={(e)=>setAmount(e.target.value)}/> 
+      <button onClick={handleIncClick}>Inc</button>
     </div>
   );
 }
